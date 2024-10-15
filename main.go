0528fdb664cfdb88e6dc2d4d86f6776e17855781
weebney <weebney@gmail.com>
2024-10-15 17:24:40 -0400

    add elimination handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -16,6 +17,28 @@ func main() {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./public"), false))
 		return nil
 	})
+
+	// Elimination handler --- gets new target when eliminated
+	app.OnRecordBeforeUpdateRequest("players").Add(func(e *core.RecordUpdateEvent) error {
+		if e.Record.OriginalCopy().GetBool("is_alive") && !e.Record.GetBool("is_alive") {
+			selfID := e.Record.GetString("user")
+			gameID := e.Record.GetString("game")
+			assassin, err := app.Dao().FindFirstRecordByFilter("players", fmt.Sprintf("target.id = '%s' && game.id = '%s'", selfID, gameID), nil)
+			if err != nil {
+				return err
+			}
+			targetID := e.Record.GetString("target")
+			assassin.Set("target", targetID)
+			if err := app.Dao().SaveRecord(assassin); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	// TODO: elimintaion confirmation
+
+	// TODO: winner handler
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-git pull
-CGO_ENABLED=0 go build -o ./assassingame_server
-./assassingame_server serve --dev
+git pull origin master
+CGO_ENABLED=0 go build -o ./assassingame_server && ./assassingame_server serve --dev
