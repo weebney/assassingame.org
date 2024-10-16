ab543be28149cac9a4c8654e38af1311ee36c17f
weebney <weebney@gmail.com>
2024-10-16 18:53:51 -0400

    going to production

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env sh
-git pull origin master
-CGO_ENABLED=0 go build -o ./assassingame_server && ./assassingame_server serve --dev
+CGO_ENABLED=0 go build -o ./assassingame_server && ./assassingame_server serve assassingame.org
